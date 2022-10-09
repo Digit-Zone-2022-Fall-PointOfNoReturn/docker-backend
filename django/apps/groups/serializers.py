fdeebb8c93a3273c1007fd013e94450e18d0e0e8
Helltraitor <helltraitor@hotmail.com>
2022-10-09 14:01:24 +0300

    Fixed fields using in serializers

--- django/apps/groups/serializers.py
+++ django/apps/groups/serializers.py
@@ -6,14 +6,12 @@ from .models import Cart, Group, GroupMember
 class GroupSerializer(ModelSerializer):
     class Meta:
         model = Group
-        fields = '__all__'
         read_only_fields = ['id']
 
 
 class PostGroupsSerializer(ModelSerializer):
     class Meta:
         model = Group
-        fields = '__all__'
         exclude = ['id']
 
 
@@ -23,12 +21,10 @@ PutGroupsSerializer = PostGroupsSerializer
 class GroupMemberSerializer(ModelSerializer):
     class Meta:
         model = GroupMember
-        fields = '__all__'
         read_only_fields = ['group', 'user']
 
 
 class CartSerializer(ModelSerializer):
     class Meta:
         model = Cart
-        fields = '__all__'
         read_only_fields = ['group', 'user', 'store', 'product']
