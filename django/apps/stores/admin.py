19a5e64e50b59d029d784b2d8208a496335929a0
Helltraitor <helltraitor@hotmail.com>
2022-10-09 13:45:17 +0300

    Updated admin panel

--- django/apps/stores/admin.py
+++ django/apps/stores/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
+from django.http import HttpRequest
 
 from .models import Product, Store
 
 
 admin.site.register(Product)
 admin.site.register(Store)
+
+@admin.register(Product)
+class ProductView(admin.ModelAdmin):
+    def get_readonly_fields(self, request: HttpRequest, obj: Product = None):
+        return [] if obj is None else ['id', 'store']
+
+
+@admin.register(Store)
+class StoreView(admin.ModelAdmin):
+    def get_readonly_fields(self, request: HttpRequest, obj: Store = None):
+        return [] if obj is None else ['id']
--- django/apps/stores/models.py
+++ django/apps/stores/models.py
@@ -7,19 +7,23 @@ class Store(Model):
     class Meta:
         ordering = ['name']
 
-    id = UUIDField(editable=False, default=uuid.uuid4, primary_key=True)
+    id = UUIDField(default=uuid.uuid4, primary_key=True)
     address = TextField(blank=False)
     name = CharField(blank=False, max_length=255)
 
+    def __str__(self) -> str:
+        return str(self.name)
+
 
 class Product(Model):
-    class Meta:
-        ordering = ['name']
-    
-    id = UUIDField(editable=False, default=uuid.uuid4, primary_key=True)
+    id = UUIDField(default=uuid.uuid4, primary_key=True)
     store = ForeignKey(Store, on_delete=CASCADE)
     name = CharField(blank=False, max_length=255)
     description = TextField(blank=True)
     # Price for one item bound by million units
     price = DecimalField(max_digits=7, decimal_places=2)
     discount = JSONField(blank=True, default=dict)
+
+    def __str__(self) -> str:
+        store = Store.objects.get(id=self.store)
+        return f'{store.name} {self.name}'
